58a9f75b3f45171b4de1cc6472794bb9f74d7f3c
bchen528 <bchen720@gmail.com>
2018-04-09 20:31:35 +0000

    delete space after print

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print ("{:s}{:s}{:s}".format(str, str, str))
-print ("{:s}".format(str[:9]))
+print("{:s}{:s}{:s}".format(str, str, str))
+print("{:s}".format(str[:9]))
