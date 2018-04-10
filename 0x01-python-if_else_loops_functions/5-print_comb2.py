87aed6e9a653b22534ef604738c0ae7c24377848
bchen528 <bchen720@gmail.com>
2018-04-10 19:09:33 +0000

    fix print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x < 10:
-        print("{:d}{:d}".format(0, x), end=", ")
+    if x != 99:
+        if x < 10:
+            print("0", end="")
+        print("{:d}".format(x), end=", ")
     else:
-        if x != 99:
-            print("{:d}".format(x), end=", ")
-        else:
-            print("99")
+        print("{:d}".format(x))
