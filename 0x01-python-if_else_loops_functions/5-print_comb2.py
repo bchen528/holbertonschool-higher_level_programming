f5a9d4804817999f794c8ca509e368cffcbc6df7
bchen528 <bchen720@gmail.com>
2018-04-10 19:52:20 +0000

    adjust format string

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
     if x != 99:
-        if x < 10:
-            print("0", end="")
-        print("{:d}".format(x), end=", ")
+        print("{:02d}".format(x), end=", ")
     else:
         print("{:d}".format(x))
