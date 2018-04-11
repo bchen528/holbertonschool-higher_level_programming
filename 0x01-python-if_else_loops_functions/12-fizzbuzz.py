e9027baf28000b3ebaf23360b9cbf16be9f2c0fb
bchen528 <bchen720@gmail.com>
2018-04-11 00:11:31 +0000

    add space at end

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,9 +6,6 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
-            if i != 100:
                 print("Buzz", end=" ")
-            else:
-                print("Buzz", end="")
         else:
             print("{:d}".format(i), end=" ")
