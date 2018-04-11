d64923a4d79fe83826cfa5b0dbfd4777f7e9e0ff
bchen528 <bchen720@gmail.com>
2018-04-11 00:09:43 +0000

    change to for loop

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    i = 1
-    while i < 101:
+    for i in range(1, 101):
         if i % 15 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
@@ -13,4 +12,3 @@ def fizzbuzz():
                 print("Buzz", end="")
         else:
             print("{:d}".format(i), end=" ")
-        i = i + 1
