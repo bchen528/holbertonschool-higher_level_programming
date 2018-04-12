062fb1ee0b04800eaa5a5be52924da0a80fc3992
bchen528 <bchen720@gmail.com>
2018-04-12 17:33:45 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    i = 1
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+        print("{:d}: {:s}".format(i, argv[i]))
+
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
