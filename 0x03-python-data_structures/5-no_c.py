b353a4bd87ec33857e2739e8c21a4fde3c2d4125
bchen528 <bchen720@gmail.com>
2018-04-17 21:07:25 +0000

    correct output

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,8 @@ def no_c(my_string):
     s = ""
     copy = list(my_string)
     for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            del copy[i]
-    my_string = s.join(copy)
-    return my_string
+        if my_string[i] == 'c':
+            copy.remove('c')
+        if my_string[i] == 'C':
+            copy.remove('C')
+    return s.join(copy)
