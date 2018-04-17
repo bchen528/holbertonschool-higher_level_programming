85654ff393f85ef417713ab65df4ac80326b51fb
bchen528 <bchen720@gmail.com>
2018-04-17 19:48:19 +0000

    add list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    copy = list(my_list)
+    copy[idx] = element
+    return copy
