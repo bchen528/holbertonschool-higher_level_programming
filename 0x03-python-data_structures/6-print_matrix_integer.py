cfcff898e4e5635abb3774e8755bdc8b3148f755
bchen528 <bchen720@gmail.com>
2018-04-17 21:31:31 +0000

    generalize

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if j == 2:
+            if j == len(matrix[i]) - 1:
                 print("{:d}".format(matrix[i][j]), end="")
             else:
                 print("{:d} ".format(matrix[i][j]), end="")
