b08eeb0c6accca90d718bac07bbf684a0babcf59
bchen528 <bchen720@gmail.com>
2018-04-18 01:07:17 +0000

    reorder condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list == "":
         return None
     for i in range(len(my_list)):
-        if (i == 0):
+        if i == 0:
             largestNum = my_list[i]
             i = i + 1
-        if my_list[i] > largestNum and i < len(my_list):
+        if i < len(my_list) and my_list[i] > largestNum:
             largestNum = my_list[i]
     return largestNum
