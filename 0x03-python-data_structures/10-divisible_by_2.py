7ce2b061cff0646930127d21260f869a19760a4b
bchen528 <bchen720@gmail.com>
2018-04-18 00:28:09 +0000

    add null check

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new = list(my_list)
-    for i in my_list:
-        if i % 2 == 0:
-            new[i] = True
-        else:
-            new[i] = False
-    return new
+    if my_list is not None:
+        new = list(my_list)
+        for i in my_list:
+            if i % 2 == 0:
+                new[i] = True
+            else:
+                new[i] = False
+        return new
