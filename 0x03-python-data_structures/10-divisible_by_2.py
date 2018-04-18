4826f62f7e1c45efcf7a0d258da2c4af3dec4439
bchen528 <bchen720@gmail.com>
2018-04-18 00:36:06 +0000

    change loop range

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,10 @@
 def divisible_by_2(my_list=[]):
     if my_list is not None:
         new = list(my_list)
-        for i in my_list:
-            if i % 2 == 0:
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
                 new[i] = True
             else:
                 new[i] = False
+        print (new)
         return new
