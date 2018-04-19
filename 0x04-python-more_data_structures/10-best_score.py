9313b1e8e0ad089d6374b3368b04c9385c77dcae
bchen528 <bchen720@gmail.com>
2018-04-19 20:07:48 +0000

    correct missing variable assignment

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,7 +8,7 @@ def best_score(a_dictionary):
     for key in a_dictionary:
         if flag == 1:
             largestVal = a_dictionary[key]
-            largestKey
+            largestKey = key
             flag = 0
         if a_dictionary[key] > largestVal:
             largestVal = a_dictionary[key]
