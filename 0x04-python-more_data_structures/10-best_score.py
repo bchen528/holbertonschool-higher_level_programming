ddca4b0f8e6a6b9f7010d3ddf3f87c892b660d7f
bchen528 <bchen720@gmail.com>
2018-04-19 20:06:26 +0000

    rename variable

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,10 +7,10 @@ def best_score(a_dictionary):
     largestKey = ""
     for key in a_dictionary:
         if flag == 1:
-            largestNum = a_dictionary[key]
+            largestVal = a_dictionary[key]
             largestKey
             flag = 0
-        if a_dictionary[key] > largestNum:
-            largestNum = a_dictionary[key]
+        if a_dictionary[key] > largestVal:
+            largestVal = a_dictionary[key]
             largestKey = key
     return largestKey
