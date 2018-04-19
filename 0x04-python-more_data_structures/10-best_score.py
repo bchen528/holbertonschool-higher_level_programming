1816a87fccb2d8d899810e47c6b30fa7e2d88645
bchen528 <bchen720@gmail.com>
2018-04-19 20:04:03 +0000

    remove else

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        flag = 1
-        largestVal = 0
-        largestKey = ""
-        for key in a_dictionary:
-            if flag == 1:
-                largestNum = a_dictionary[key]
-                largestKey
-                flag = 0
-            if a_dictionary[key] > largestNum:
-                largestNum = a_dictionary[key]
-                largestKey = key
-        return largestKey
-    else:
+    if a_dictionary is None:
         return None
+    flag = 1
+    largestVal = 0
+    largestKey = ""
+    for key in a_dictionary:
+        if flag == 1:
+            largestNum = a_dictionary[key]
+            largestKey
+            flag = 0
+        if a_dictionary[key] > largestNum:
+            largestNum = a_dictionary[key]
+            largestKey = key
+    return largestKey
