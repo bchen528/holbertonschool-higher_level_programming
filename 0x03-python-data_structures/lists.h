67d1ae95bbe1716af1071fe278036c9c088b4098
bchen528 <bchen720@gmail.com>
2018-04-19 05:56:23 +0000

    add prototypes

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,7 +19,8 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
 int is_palindrome(listint_t **head);
-
+int _strlen_recursion(char *s);
+int check_palin(char *s, int start, int end);
+int is_palin(char *s);
 #endif /* LISTS_H */
