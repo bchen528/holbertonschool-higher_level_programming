187a256d5b9b6d02bd7556247cb75d448367fc4a
bchen528 <bchen720@gmail.com>
2018-04-19 22:49:15 +0000

    change null statement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if roman_string is not None::
         return None
 
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
