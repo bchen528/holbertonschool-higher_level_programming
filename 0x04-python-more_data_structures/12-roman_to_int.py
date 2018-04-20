5dbda8a2715200feaf623ff85c6bdd6ce19bbe00
bchen528 <bchen720@gmail.com>
2018-04-20 02:29:17 +0000

    change null check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string == "":
+    if roman_string is None or type(roman_string) is not str:
         return 0
     for j in roman_string:
         if (j != 'I' and j != 'V' and j != 'X' and
