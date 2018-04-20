ded13f11730a905a1e20506e13bc9db90f0c2a96
bchen528 <bchen720@gmail.com>
2018-04-20 05:33:31 +0000

    first commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    result = []
+    weights = []
+
+    for tup in my_list:
+        result.append(tup[0] * tup[1])
+        weights.append(tup[1])
+
+    return sum(result) / sum(weights)
