9b4f259db852984b4b3ebee5eb1798fcdd6cf275
bchen528 <bchen720@gmail.com>
2018-04-20 02:02:03 +0000

    rework logic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,41 +2,16 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return None
-
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    flag = 0
     num = []
-
-    for i in range(len(roman_string)):
-        if flag == 1:
-            if roman_string[i] == 'I':
-                num.append(d['I'] + d['I'])
-            elif roman_string[i] == 'V':
-                num.append(d['V'] - d['I'])
-            elif roman_string[i] == 'X':
-                num.append(d['X'] - d['I'])
-            elif roman_string[i] == 'L':
-                num.append(d['L'] - d['I'])
-            elif roman_string[i] == 'C':
-                num.append(d['C'] - d['I'])
-            elif roman_string[i] == 'D':
-                num.append(d['D'] - d['I'])
-            elif roman_string[i] == 'M':
-                num.append(d['M'] - d['I'])
-            flag = 0
+    for i in range(len(roman_string) - 1, -1, -1):
+        if i == len(roman_string) - 1:
+            largestNum = d[roman_string[i]]
+        if d[roman_string[i]] < largestNum:
+            num.append(largestNum - d[roman_string[i]])
+            num.remove(largestNum)
+            largestNum = d[roman_string[i]]
         else:
-            if roman_string[i] == 'I':
-                flag = 1
-            elif roman_string[i] == 'V':
-                num.append(d['V'])
-            elif roman_string[i] == 'X':
-                num.append(d['X'])
-            elif roman_string[i] == 'L':
-                num.append(d['L'])
-            elif roman_string[i] == 'C':
-                num.append(d['C'])
-            elif roman_string[i] == 'D':
-                num.append(d['D'])
-            elif roman_string[i] == 'M':
-                num.append(d['M'])
+            num.append(d[roman_string[i]])
+            largestNum = d[roman_string[i]]
     return sum(num)
