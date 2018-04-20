423696462dcb8759f48bfd8671d573ad61ffb85d
bchen528 <bchen720@gmail.com>
2018-04-20 02:05:33 +0000

    correct null check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None:
-        return None
+        return 0
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = []
     for i in range(len(roman_string) - 1, -1, -1):
