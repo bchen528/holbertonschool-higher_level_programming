f4ff2fed5c3bad79a86d0e0042a4a2596d18d265
bchen528 <bchen720@gmail.com>
2018-04-30 22:24:48 +0000

    fix exception clause

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-    except IndexError:
-        print("IndexError: list index out of range")
-    except:
+    except ValueError and TypeError:
         return
     else:
         print()
