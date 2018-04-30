23389225cd59e7155c4ed36b5ba0e282f621611b
bchen528 <bchen720@gmail.com>
2018-04-30 21:25:27 +0000

    first commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+    except IndexError:
+        print("IndexError: list index out of range")
+    except:
+        return
+    else:
+        print()
+        return count
