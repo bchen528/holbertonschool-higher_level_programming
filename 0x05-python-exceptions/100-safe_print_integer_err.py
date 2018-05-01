6bc1212a18b0c974c265e268c01d553b9b1e656f
bchen528 <bchen720@gmail.com>
2018-05-01 00:36:08 +0000

    generalize error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except:
-        print("Exception: Unknown format code 'd' for object of type 'str'",
-              file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr);
         return False
     return True
