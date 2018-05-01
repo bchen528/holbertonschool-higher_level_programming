ea9b48e79fa688d801fe369e58c9d76e9314779c
bchen528 <bchen720@gmail.com>
2018-05-01 00:36:56 +0000

    remove semicolon

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr);
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
     return True
