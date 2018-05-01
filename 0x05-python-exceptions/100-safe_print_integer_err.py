948fc55ec605f6211d8565fb690bb03220f39993
bchen528 <bchen720@gmail.com>
2018-05-01 00:24:33 +0000

    remove else

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,5 +9,4 @@ def safe_print_integer_err(value):
         print("Exception: Unknown format code 'd' for object of type 'str'",
               file=sys.stderr)
         return False
-    else:
-        return True
+    return True
