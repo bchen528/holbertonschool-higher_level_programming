461f1ff9a4b40c6cee5642151f60c1fe19001c74
bchen528 <bchen720@gmail.com>
2018-05-01 00:07:46 +0000

    remove extra text in print message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("Exception: {:d}".format(value))
+        print("{:d}".format(value))
     except:
         print("Exception: Unknown format code 'd' for object of type 'str'",
               file=sys.stderr)
