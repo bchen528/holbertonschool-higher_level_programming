a24140eb925eb747fc4fbfb5e102c13c6013eff7
bchen528 <bchen720@gmail.com>
2018-05-02 04:10:20 +0000

    fix if conditions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,9 +68,8 @@ class Square:
             value (int, int): value to assign position
         """
 
-        if type(value[0]) is int and type(value[1]) is int\
-           and value[0] >= 0 and value[1] >= 0 and len(value) == 2\
-           and type(value) is tuple:
+        if type(value) is tuple and len(value) == 2 and type(value[0]) is int\
+           and type(value[1]) is int and value[0] >= 0 and value[1] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
