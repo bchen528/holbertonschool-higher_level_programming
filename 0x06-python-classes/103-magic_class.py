6e44045a5ac33de3b0a30771e1391d2e598ad3c9
bchen528 <bchen720@gmail.com>
2018-05-02 06:47:44 +0000

    remove args

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import math
+
+
 class _MagicClass:
     """creates a protected class, MagicClass"""
+
     def __init__(self, radius=0):
-        """initialized attributes for instance of MagicClass
-        Args:
-            radius (int): radius
-        """
+        """initialized attributes for instance of MagicClass"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
@@ -14,8 +14,11 @@ class _MagicClass:
 
     def area(self):
         """calculates area of circle"""
-        return (self.__radius**2) * math.pi
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
         """calculates circumference of circle"""
         return 2 * math.pi * self.__radius
+
+import dis
+dis.dis(_MagicClass)
