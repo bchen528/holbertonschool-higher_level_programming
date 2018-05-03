8209e2282023970fa432d2ee07c92d522d565f8a
bchen528 <bchen720@gmail.com>
2018-05-03 21:09:51 +0000

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Adding integers
+"""
+
+
+def add_integer(a, b=98):
+    """ adding two integers
+    Args:
+        a (int): integer value
+        b (int): integer value if specified, otherwise 98
+    Returns:
+        sum of two integers
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
