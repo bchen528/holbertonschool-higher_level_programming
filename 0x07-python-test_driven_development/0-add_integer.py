cb8cd6dd790555f025f455eb29c4d3f2651fb23b
bchen528 <bchen720@gmail.com>
2018-05-06 16:47:24 +0000

    add Nan and inf test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,8 +14,8 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if either a or b are not integers
     """
-    if type(a) != int and type(a) != float:
+    if a is None or type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
