909511dd4088d76b873d2f44a7cde9d980fe53d1
bchen528 <bchen720@gmail.com>
2018-05-07 18:21:22 +0000

    first commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Rectangle:
+    """creates a class Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
