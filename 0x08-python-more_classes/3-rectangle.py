048c4944b508e07e65de72d374f5629fcb9db6c5
bchen528 <bchen720@gmail.com>
2018-05-07 20:20:26 +0000

    add documentation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -2,15 +2,28 @@
 class Rectangle:
     """creates a class Rectangle"""
     def __init__(self, width=0, height=0):
+        """initialize width and height of rectangle
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
         self.width = width
         self.height = height
 
     @property
     def height(self):
+        """get height
+        Returns:
+            height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """set height
+        Args:
+            value (int): height value
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -19,10 +32,18 @@ class Rectangle:
 
     @property
     def width(self):
+        """get width
+        Returns:
+            width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """set width
+        Args:
+            value (int): width
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,14 +51,26 @@ class Rectangle:
         self.__width = value
 
     def area(self):
+        """find area of rectangle
+        Returns:
+            area of rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """find perimeter of rectangle
+        Returns:
+            perimeter of rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """print square
+        Returns:
+            string to be printed to stdout
+        """
         new = ""
         for i in range(self.__height):
             for j in range(self.__width):
