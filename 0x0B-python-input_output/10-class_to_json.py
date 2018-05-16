57e4965805833b75dbb49d5e9b446a5b0ba0aef7
bchen528 <bchen720@gmail.com>
2018-05-16 22:08:41 +0000

    first commit

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""JavaScript Object Notation"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean) for JSON serialization
+        of an object
+    Args:
+        obj(obj): object
+    Returns:
+        dictionary description for JSON serialization of an object
+    """
+    return obj.__dict__
