c4bdd44fb0afc2c8350ae9576d8dbc14d367bfa3
bchen528 <bchen720@gmail.com>
2018-05-16 20:49:17 +0000

    first commit

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""JavaScript Object Notation"""
+
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file
+    Args:
+        filename(str): filename
+    """
+    with open(filename, mode="r", encoding="utf-8") as a_file:
+        return json.load(a_file)
