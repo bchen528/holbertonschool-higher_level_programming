e1806e70897f6b9c29ef5687f711c71cac0f15f9
bchen528 <bchen720@gmail.com>
2018-05-16 23:42:50 +0000

    first commit

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class Student:
+    """creates a class Student"""
+    def __init__(self, first_name, last_name, age):
+        """initialize instance attributes
+        Args:
+            first_name(str): first name
+            last_name(str): last name
+            age(int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        Args:
+            attrs(list): a list of attributes
+        Returns:
+            a dictionary representation of a Student instance
+        """
+        new = {}
+        i = 0
+        count = 0
+        if type(attrs) is list:
+            for key in attrs:
+                if key in self.__dict__:
+                    new[key] = self.__dict__[key]
+            return new
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance
+        Args:
+            json(dict): a dictionary
+        """
+        self.__dict__ = json
