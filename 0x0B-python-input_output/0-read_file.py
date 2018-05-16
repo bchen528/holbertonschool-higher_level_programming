0924e23afdbfd87aa28d1fd0e14fcedea64d1b2f
bchen528 <bchen720@gmail.com>
2018-05-16 19:47:08 +0000

    add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """read file contents
+    Args:
+        filename(str): filename
+    """
     with open(filename, mode="r", encoding="utf-8") as a_file:
         print(a_file.read(), end="")
