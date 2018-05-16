34754a635f5e260679cba306cb80ad8e2759df07
bchen528 <bchen720@gmail.com>
2018-05-16 20:50:14 +0000

    add documentation

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -7,6 +7,8 @@ def load_from_json_file(filename):
     """creates an Object from a JSON file
     Args:
         filename(str): filename
+    Returns:
+        returns object created from JSON file
     """
     with open(filename, mode="r", encoding="utf-8") as a_file:
         return json.load(a_file)
