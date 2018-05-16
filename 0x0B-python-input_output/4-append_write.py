25794ddf2e88cd8cf5ea12171e56ec4ffef23501
bchen528 <bchen720@gmail.com>
2018-05-16 20:04:01 +0000

    first commit

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """append a string at the end of text file
+    Args:
+        filename(str): filename
+        text(str): text to be appended
+    Returns:
+        number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as a_file:
+        a_file.write(text)
+        return len(text)
