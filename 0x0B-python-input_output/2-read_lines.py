a66fb2be5b381a6ea9aec4e53f7c307508e9d894
bchen528 <bchen720@gmail.com>
2018-05-16 19:41:12 +0000

    add documentation

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    """reads n lines of a text file
+    Args:
+        filename(str): filename
+        nb_lines(int): number of lines in file
+    """
     i = 0
     with open(filename, mode="r", encoding="utf-8") as a_file:
         if nb_lines <= 0:
