c086e4f5885a23d7fc0f38c14ca7cad2f3feaa41
bchen528 <bchen720@gmail.com>
2018-05-16 23:13:27 +0000

    first commit

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+"""access commandline arguments"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
+
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except:
+    content = []
+
+for i in range(1, len(argv)):
+    content.append(argv[i])
+print(content)
+save_to_json_file(content, filename)
