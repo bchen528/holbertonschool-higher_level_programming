c230f3843df32ebbe2dffd8fba1fb592b61b4daf
bchen528 <bchen720@gmail.com>
2018-05-16 19:44:14 +0000

    add documentation

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
+    """find number of lines in file
+    Args:
+        filename(str): filename
+    Returns:
+        number of lines in file
+    """
     line_count = 0
     with open(filename, mode="r", encoding="utf-8") as a_file:
         for a_line in a_file:
