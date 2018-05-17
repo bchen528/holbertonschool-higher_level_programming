abb6605176b5dcd9e390784605e33619f9066277
bchen528 <bchen720@gmail.com>
2018-05-17 07:49:20 +0000

    remove import

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import json
-"""JavaScript Object Notation"""
-
-
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
         (list, dictionary, string, integer and boolean) for JSON serialization
