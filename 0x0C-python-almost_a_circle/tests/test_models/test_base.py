e8b2490cd1fdf66a0d7e7b02099a53035358fc4e
bchen528 <bchen720@gmail.com>
2018-05-22 06:16:48 +0000

    add documentation and pep8 check

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,9 +2,12 @@
 import unittest
 """unittest"""
 from models.base import Base
-"""base class"""
+"""Base class"""
 from models.rectangle import Rectangle
+"""Rectangle class"""
 import json
+"""JavaScript Object Notation"""
+
 
 class TestBase(unittest.TestCase):
     """class TestBase"""
@@ -23,16 +26,16 @@ class TestBase(unittest.TestCase):
 
     def test_toJsonString(self):
         r1 = Rectangle(10, 7, 2, 8)
-        a_dict = r1.to_dictionary() # dict
-        json_string = json.dumps([a_dict]) # str of list dict
-        json_listdict = r1.to_json_string([a_dict]) # str of list dict
+        a_dict = r1.to_dictionary()  # dict
+        json_string = json.dumps([a_dict])  # str of list dict
+        json_listdict = r1.to_json_string([a_dict])  # str of list dict
         self.assertTrue(json_string == json_listdict)
 
     def test_saveToFile(self):
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
-        a_dict = [r1.to_dictionary(), r2.to_dictionary()] # dict
+        a_dict = [r1.to_dictionary(), r2.to_dictionary()]  # list dict
         Rectangle.save_to_file([r1, r2])
         with open("Rectangle.json", "r") as file:
-            list_dict = json.loads(file.read()) # list of dict
+            list_dict = json.loads(file.read())  # list dict
         self.assertTrue(a_dict == list_dict)
