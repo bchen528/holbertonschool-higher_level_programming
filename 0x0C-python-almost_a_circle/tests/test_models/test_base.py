d3004e166bcc97b89390aa89a6e132ba9e9aa6ee
bchen528 <bchen720@gmail.com>
2018-05-22 06:07:12 +0000

    update unit tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,7 +3,8 @@ import unittest
 """unittest"""
 from models.base import Base
 """base class"""
-
+from models.rectangle import Rectangle
+import json
 
 class TestBase(unittest.TestCase):
     """class TestBase"""
@@ -16,5 +17,22 @@ class TestBase(unittest.TestCase):
         self.assertIsInstance(b2, Base)
 
     def test_numObj(self):
+        Base._Base__nb_objects = 0
         b3 = Base()
-        self.assertEqual(b3.id, 3)
+        self.assertEqual(b3.id, 1)
+
+    def test_toJsonString(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        a_dict = r1.to_dictionary() # dict
+        json_string = json.dumps([a_dict]) # str of list dict
+        json_listdict = r1.to_json_string([a_dict]) # str of list dict
+        self.assertTrue(json_string == json_listdict)
+
+    def test_saveToFile(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        a_dict = [r1.to_dictionary(), r2.to_dictionary()] # dict
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            list_dict = json.loads(file.read()) # list of dict
+        self.assertTrue(a_dict == list_dict)
