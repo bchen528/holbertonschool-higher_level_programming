277f29c42a960f05c70cc43d993d5d13f52ed713
bchen528 <bchen720@gmail.com>
2018-05-22 16:59:29 +0000

    add square create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,7 +65,10 @@ class Base:
         Returns:
             an instance with all attributes already set
         """
-        dummy = cls(1, 1, 1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1, 1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1, 1, 1)
         dummy.update(**dictionary)
         return dummy
 
