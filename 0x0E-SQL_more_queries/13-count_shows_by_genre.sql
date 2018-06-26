e861c482a90ee5a4f255bcc59ea31f1586c2f73a
bchen528 <bchen720@gmail.com>
2018-06-26 22:28:38 +0000

    group by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
 LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY genre
 ORDER BY number_shows DESC;
