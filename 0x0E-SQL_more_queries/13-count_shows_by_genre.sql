868b7d25dabf2ca4fe876d80cb5b29a31ed04b15
bchen528 <bchen720@gmail.com>
2018-06-26 22:27:06 +0000

    change name to genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_genres.name, COUNT(*) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
 LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
