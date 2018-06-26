af4cdeb1aa73f99f36aefb76ab111fa2d2cde6b1
bchen528 <bchen720@gmail.com>
2018-06-26 22:23:39 +0000

    first commit

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name, COUNT(*) AS number_shows
+FROM tv_genres
+LEFT OUTER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_shows DESC;
