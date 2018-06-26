eb4f7108810d26c149da31f41b27e42b34cbc1d5
bchen528 <bchen720@gmail.com>
2018-06-26 23:22:18 +0000

    sort by genre name

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,4 +7,5 @@ LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 LEFT OUTER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name);
+ORDER BY tv_genres.name)
+ORDER BY tv_genres.name;
