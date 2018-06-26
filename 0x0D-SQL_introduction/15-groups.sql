bbf3fc5c5ce3a10d9db3a17d2d8bd5359807d806
bchen528 <bchen720@gmail.com>
2018-06-26 00:31:10 +0000

    first commit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, count(*) AS number from second_table GROUP BY score ORDER BY number DESC;
