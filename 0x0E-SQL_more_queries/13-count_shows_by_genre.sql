22ffe0fa6dbce25ba8663cb44e0961940ffb272b
bchen528 <bchen720@gmail.com>
2018-06-26 22:32:41 +0000

    add null case

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,6 @@ SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
 LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY genre
 ORDER BY number_shows DESC;
