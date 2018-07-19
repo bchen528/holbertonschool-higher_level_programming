2b49886edd06379f5e41705ee5d5ec35cbf66a3e
bchen528 <bchen720@gmail.com>
2018-07-19 18:37:31 +0000

    use argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """lists all states from the database"""
 import MySQLdb
+from sys import argv
 
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user="root", passwd="", db="hbtn_0e_0_usa")
-cur = db.cursor()
-num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-for i in range(num_rows):
-    print(cur.fetchone())
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for i in range(num_rows):
+        print(cur.fetchone())
