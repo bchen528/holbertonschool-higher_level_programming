ac70f8987915316cc854e67b20c7d963b54cf4a5
bchen528 <bchen720@gmail.com>
2018-07-20 02:01:05 +0000

    use cur.fetchall()

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-    for i in range(num_rows):
-        row = cur.fetchone()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
         if row[1][0] == 'N':
             print(row)
     cur.close()
