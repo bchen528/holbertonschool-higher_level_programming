f854415f1371f6f0fddf58fb04fe47e90e093e3e
bchen528 <bchen720@gmail.com>
2018-07-20 06:21:13 +0000

    fix output

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     session = Session(engine)
     new = State(name="Louisiana")
     session.add(new)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).filter_by(name="Louisiana").order_by(State.id).all():
+        print("{}".format(state.id))
     session.close()
