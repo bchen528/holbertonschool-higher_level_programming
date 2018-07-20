e5b05d2aba2fc0eb52793d5a0847335dec100815
bchen528 <bchen720@gmail.com>
2018-07-20 16:45:35 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""creates City class"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """class City"""
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+class State(Base):
+    """class State"""
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
