658781d65e1343731f172aa7e81980140e71039a
bchen528 <bchen720@gmail.com>
2018-07-20 02:08:20 +0000

    specify str type

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY\
-    states.id".format(argv[4]))
+    sql = "SELECT * FROM states WHERE name='{:s}'\
+    ORDER BY states.id".format(argv[4])
+    cur.execute(sql)
     rows = cur.fetchall()
     for row in rows:
         print(row)
