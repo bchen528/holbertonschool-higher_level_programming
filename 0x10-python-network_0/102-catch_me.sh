f359ec1275bf606d25ffc16483591f6f8770add5
bchen528 <bchen720@gmail.com>
2018-08-01 00:27:10 +0000

    change string

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -o /dev/null -sw "You got this!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
