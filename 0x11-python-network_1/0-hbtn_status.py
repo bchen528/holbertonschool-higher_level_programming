899fe64707fcb9e55337234242d34a22f4089d7c
bchen528 <bchen720@gmail.com>
2018-08-06 17:03:32 +0000

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# fetches https://intranet.hbtn.io/status using only urllib package
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+
+    print("Body response:")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(str(html)[2:-1]))
