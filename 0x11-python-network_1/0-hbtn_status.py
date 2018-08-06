a2586d1ce8534a72a66f6e2f9d998afe47e1ff0a
bchen528 <bchen720@gmail.com>
2018-08-06 17:20:29 +0000

    add documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# fetches https://intranet.hbtn.io/status using only urllib package
+"""fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 
 if __name__ == "__main__":
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         html_str = html.decode('utf-8')
