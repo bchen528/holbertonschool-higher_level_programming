d0acf4a6f5435211d20ec0df295d698552e00761
bchen528 <bchen720@gmail.com>
2018-08-06 17:16:44 +0000

    use decode for utf-8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,9 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
+        html_str = html.decode('utf-8')
 
     print("Body response:")
     print("    - type: {}".format(type(html)))
     print("    - content: {}".format(html))
-    print("    - utf8 content: {}".format(str(html)[2:-1]))
+    print("    - utf8 content: {}".format(html_str))
