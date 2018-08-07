986bea52f53d2321f791fac5d0d466de389e386e
bchen528 <bchen720@gmail.com>
2018-08-07 00:32:58 +0000

    first commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of the repository “rails”
+by the user “rails”
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    list 10 commits (from the most recent to oldest) of the repository
+    “rails” by the user “rails”
+    """
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+    res_list = r.json()
+    for i in range(10):
+        print("{}: {}".format(res_list[i].get('sha'), res_list[i].
+                              get('commit').get('author').get('name')))
