555643e8dd781aee65a03906cf320bd01b16cbb2
bchen528 <bchen720@gmail.com>
2018-08-28 18:18:54 +0000

    change string for multiple arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (process.argv[3]) {
+  console.log('Arguments found');
+} else if (process.argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
