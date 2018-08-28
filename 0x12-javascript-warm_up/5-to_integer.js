c9815ca8eed78ce6414757985e89768f38480ec7
bchen528 <bchen720@gmail.com>
2018-08-28 18:45:18 +0000

    add isNaN condition

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (process.argv[2] && isNaN(process.argv[2]) === false) {
   console.log('My number: ' + parseInt(process.argv[2], 10));
 } else {
   console.log('Not a number');
