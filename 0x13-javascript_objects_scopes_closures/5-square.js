67fe8d3a4e18f9418d782c3a6f659d007005939f
bchen528 <bchen720@gmail.com>
2018-09-04 18:57:49 +0000

    task 5 first commit

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      Object.create(Rectangle);
+    } else if (w && h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let y = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(y.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let i = this.width;
+    this.width = this.height;
+    this.height = i;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.name = 'Square';
+  }
+}
+
+module.exports = Square;
