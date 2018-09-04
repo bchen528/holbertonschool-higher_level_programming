eb9c52bc3f3eb973796c9ca9e748f9d9bdf43fd9
bchen528 <bchen720@gmail.com>
2018-09-04 17:48:38 +0000

    task 2 first commit

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      Object.create(Rectangle);
+    } else if (w && h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
