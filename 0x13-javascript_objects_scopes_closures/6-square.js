b7e3d247b062f86b33835943da470277dca8b16d
bchen528 <bchen720@gmail.com>
2018-09-04 23:34:23 +0000

    import 5-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      Object.create(Rectangle);
-    } else if (w && h) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let y = 'X';
-    for (let i = 0; i < this.height; i++) {
-      console.log(y.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    let i = this.width;
-    this.width = this.height;
-    this.height = i;
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
