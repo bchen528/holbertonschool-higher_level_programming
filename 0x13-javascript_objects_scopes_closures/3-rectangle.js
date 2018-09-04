234e0d4f21f141abb61cab73432277a24410458e
bchen528 <bchen720@gmail.com>
2018-09-04 17:54:38 +0000

    task 3 first commit

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      Object.create(Rectangle);
+    } else if (w && h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let y = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(y.repeat(this.width));
+    }
+  }
+};
