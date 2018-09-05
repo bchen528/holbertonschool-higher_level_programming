527ef73ffbd5ee3e7f1651742c70cfd51e4864a3
bchen528 <bchen720@gmail.com>
2018-09-05 18:27:20 +0000

    rearrange url to include id

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,30 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films';
 const episodeId = process.argv[2];
+const url = 'http://swapi.co/api/films/' + episodeId;
 
-function listCharacters (url, episodeId) {
+function listCharacters (url) {
   request(url, function (error, response, body) {
     if (error) {
       console.log(error);
     } else {
-      let jsonDict = JSON.parse(body).results;
-      for (let i = 0; i < Object.keys(jsonDict).length; i++) {
-        let epiDict = jsonDict[i];
-        if (epiDict.episode_id === parseInt(episodeId)) {
-          let charList = epiDict.characters;
-          for (let j = 0; j < charList.length; j++) {
-            request(charList[j], function (error, response, body) {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log(JSON.parse(body).name);
-              }
-            });
+      let charList = JSON.parse(body).characters;
+      for (let j = 0; j < charList.length; j++) {
+        request(charList[j], function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(JSON.parse(body).name);
           }
-        }
+        });
       }
     }
   });
 }
-listCharacters(url, episodeId);
+listCharacters(url);
