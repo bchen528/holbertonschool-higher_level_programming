7f589bbb12e3e564ec7c220ea2fadf59c54b1ae6
bchen528 <bchen720@gmail.com>
2018-09-06 18:28:48 +0000

    refactor code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+
 let oldDict = require('./101-data.js').dict;
 let newDict = {};
 Object.keys(oldDict).forEach(function (key) {
-  newDict[oldDict[key]] = [];
-});
-Object.keys(oldDict).forEach(function (key) {
-  if (newDict[oldDict[key]].includes(key) === false) {
-    newDict[oldDict[key]].push(key);
+  if (newDict[oldDict[key]] === undefined) {
+    newDict[oldDict[key]] = [];
   }
+  newDict[oldDict[key]].push(key);
 });
-console.log(newDict);
