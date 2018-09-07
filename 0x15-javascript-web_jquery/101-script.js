f1c56f707b35db991c86a1c68368471ae0bd5e8e
bchen528 <bchen720@gmail.com>
2018-09-07 17:48:02 +0000

    task 11 first commit

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
