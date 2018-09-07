f52c6e92e6d716d9977cfff4b865e20ab14fc7af
bchen528 <bchen720@gmail.com>
2018-09-07 18:27:49 +0000

    task 12 first commit

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:city_name%22)&format=json',
+      success: function (data) {
+        $('INPUT#btn_search').click(function () {
+          let input = $('INPUT#city_search').val();
+          if (input) {
+            $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
+          }
+        });
+      }
+    });
+  });
+});
