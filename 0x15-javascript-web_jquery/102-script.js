c0a529911eefc7e537f6190963b1bf88749e731a
bchen528 <bchen720@gmail.com>
2018-09-09 19:52:09 +0000

    add retrieval of actual wind speed for each city functionality

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   $(function () {
-    $.ajax({
-      type: 'GET',
-      url: 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:city_name%22)&format=json',
-      success: function (data) {
-        $('INPUT#btn_search').click(function () {
+    $('INPUT#btn_search').click(function () {
+      $.ajax({
+        type: 'GET',
+        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + $('INPUT#city_search').val() + '%22)&format=json',
+        success: function (data) {
           let input = $('INPUT#city_search').val();
           if (input) {
             $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
           }
-        });
-      }
+        }
+      });
     });
   });
 });
